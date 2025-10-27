c40a4a3142b460970ec13cfe37a2747eadbdb7d1
saiprakashjaihanuman-ops <saiprakash.jaihanuman@gmail.com>
2025-10-27 23:34:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -374,7 +374,16 @@ function renderProductsByCategory(category) {
     });
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.getElementById('nav-links');
+    const links = document.querySelectorAll('.nav-links a');
 
+    // Toggle menu open/close
     hamburger.addEventListener('click', () => {
       navLinks.classList.toggle('active');
+    });
+
+    // Close menu when a link is clicked
+    links.forEach(link => {
+      link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+      });
     });
